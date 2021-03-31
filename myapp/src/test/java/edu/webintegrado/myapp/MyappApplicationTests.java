1ceb3755660559676dce35a57f927abe8954c950
dmunoz23 <dmunoz3@outlook.es>
2021-03-30 20:39:41 -0500

    master

---
+++ myapp/src/test/java/edu/webintegrado/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.webintegrado.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
